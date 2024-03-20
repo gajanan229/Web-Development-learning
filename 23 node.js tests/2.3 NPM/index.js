51e5d7c23a10a84e872dd6699202367529bbe026
gajanan229 <gajanan.vig@gmail.com>
2024-03-20 14:10:43 -0400

    Add files via upload

---
+++ 23 node.js tests/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+import generateName from 'sillyname';
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+import superheroes from "superheroes";
+
+var superh = superheroes.random();
+console.log(`My name is ${superh}.`);
