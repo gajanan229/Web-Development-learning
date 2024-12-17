c9b062f1314454cbf8204c40516941a0fde9a6ae
Gajanan Vigneswaran <gajanan.vig@gmail.com>
2024-12-17 15:01:26 -0500

    Added 'Section 16 DOM' to the repository

---
+++ Section 16 DOM/DOM Challenge Starting Files/index.js
@@ -0,0 +1,14 @@
+
+var heading = document.firstElementChild.lastElementChild.firstElementChild;
+heading.innerHTML = "Good Bye";
+
+heading.style.color = "red";
+
+document.querySelector("input").click();
+
+document.querySelector("ul").lastElementChild.innerHTML = "hi";
+
+document.getElementsByTagName("li")[1].style.color = "green";
+
+document.querySelector("button").style.backgroundColor  = "yellow";
+
