3f96ff62c83a802e4bceac217f8c0d91e8c4da21
Gajanan Vigneswaran <gajanan.vig@gmail.com>
2024-12-17 14:55:59 -0500

    Moved folders into Old Learning Sections

--- 23 node.js tests/2.3 NPM/index.js
+++
@@ -1,9 +0,0 @@
-import generateName from 'sillyname';
-var sillyName = generateName();
-
-console.log(`My name is ${sillyName}.`);
-
-import superheroes from "superheroes";
-
-var superh = superheroes.random();
-console.log(`My name is ${superh}.`);
