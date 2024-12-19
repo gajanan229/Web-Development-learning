f961ea6ca68b04df320d2402d70deb969fcf464c
Gajanan Vigneswaran <gajanan.vig@gmail.com>
2024-12-19 15:57:09 -0500

    Added 'Section 23 Node.js' to the repository

---
+++ Section 23 Node.js/23.3 NPM/index.js
@@ -0,0 +1,9 @@
+import generateName from "sillyName";
+var sillyName = generateName();
+
+console.log(sillyName);
+
+import {randomSuperhero} from "superheroes";
+var superhero = randomSuperhero();
+
+console.log(superhero);
---
+++ Section 23 Node.js/23.4 QR Code Project/index.js
@@ -0,0 +1,33 @@
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+import inquirer from 'inquirer';
+import fs from "fs";
+import qr from "qr-image";
+
+
+inquirer
+    .prompt([
+        {message: "enter url for the qr code: ", 
+        name: "URL"}
+    ])
+    .then((answers) => {
+        const url = answers.URL;
+        
+        var qr_url = qr.image(url, { type: 'png' });
+        qr_url.pipe(fs.createWriteStream('URL.png'));
+        
+        fs.writeFile("URL.txt", url, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        } else {
+        // Something else went wrong
+        }
+    });
