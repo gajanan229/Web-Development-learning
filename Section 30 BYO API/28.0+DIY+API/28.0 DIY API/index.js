0c85fc56557d1bdd37295e4eeed8272043d44371
Gajanan Vigneswaran <gajanan.vig@gmail.com>
2025-01-06 06:46:32 -0500

    finnished files in 'Section 30 BYO API'

--- Section 30 BYO API/28.0+DIY+API/28.0 DIY API/index.js
+++ Section 30 BYO API/28.0+DIY+API/28.0 DIY API/index.js
@@ -22,20 +22,78 @@ app.get("/jokes/:id", (req, res) => {
 
 //3. GET a jokes by filtering on the joke type
 app.get("/filter", (req, res) => {
-  const type = parseInt(req.query.type);
+  const type = req.query.type;
   const filterJoke = jokes.filter((joke) => joke.jokeType === type);
   res.json(filterJoke);
 })
 
 //4. POST a new joke
+app.post("/jokes", (req, res) => {
+  const newJoke = {
+    id: jokes.length + 1,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  }
+  jokes.push(newJoke);
+  res.json(newJoke);
+})
 
 //5. PUT a joke
+app.put("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const repJoke = {
+    id: id,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  }
+
+  const ixdJoke = jokes.findIndex((joke) => joke.id === id);
+  jokes[ixdJoke] = repJoke
+
+  res.json(repJoke);
+})
 
 //6. PATCH a joke
+app.patch("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const ixdJoke = jokes.findIndex((joke) => joke.id === id);
+  const joke = jokes[ixdJoke];
+  const repJoke = {
+    id: id,
+    jokeText: req.body.text || joke.jokeText,
+    jokeType: req.body.type || joke.jokeType,
+  }
+
+  jokes[ixdJoke] = repJoke
+
+  res.json(repJoke);
+})
 
 //7. DELETE Specific joke
+app.delete("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const ixdJoke = jokes.findIndex((joke) => joke.id === id);
+  if (searchIndex > -1) {
+    jokes.splice(searchIndex, 1);
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `Joke with id: ${id} not found. No jokes were deleted.` });
+  }
+})
 
 //8. DELETE All jokes
+app.delete("/all", (req, res) => {
+  if (req.query.key === masterKey){
+    jokes = [];
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `Incorrect key` });
+  }
+})
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
--- Section 30 BYO API/28.1+Blog+API+Project/index.js
+++ Section 30 BYO API/28.1+Blog+API+Project/index.js
@@ -41,14 +41,55 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Write your code here//
 
 //CHALLENGE 1: GET All posts
+app.get('/posts', (req,res) => {
+  res.json(posts);
+})
 
 //CHALLENGE 2: GET a specific post by id
+app.get('/posts/:id', (req,res) => {
+  const id = parseInt(req.params.id);
+  const postG = posts.find((post) => post.id === id);
+  res.json(postG);
+})
 
 //CHALLENGE 3: POST a new post
+app.post('/posts', (req,res) => {
+  lastId += 1;
+  const newPost = {
+    id: lastId,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    date: new Date(),
+  };
+  posts.push(newPost);
+  res.status(201).json(newPost);
+})
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
+app.patch('/posts/:id', (req,res) => {
+  const id = parseInt(req.params.id);
+  const postIdx = posts.findIndex((post) => post.id === id);
+  if (postIdx === -1) return res.status(404).json({ message: "Post not found" });
+  const currPost = posts[postIdx]
+  const newPost = {
+    id: id,
+    title: req.body.title || currPost.title,
+    content: req.body.content || currPost.content,
+    author: req.body.author || currPost.author,
+    date: new Date(),
+  };
+  posts[postIdx] = newPost;
+  res.status(201).json(newPost);
+})
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+app.delete('/posts/:id', (req,res) => {
+  const postIdx = posts.findIndex((post) => post.id === parseInt(req.params.id));
+  if (postIdx === -1) return res.status(404).json({ message: "Post not found" });
+  posts.splice(postIdx,1);
+  res.json({ message: "Post deleted" });
+})
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
